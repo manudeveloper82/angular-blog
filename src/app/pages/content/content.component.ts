4a2bf344e9de05f0e15886a2ee6568001f4ba5ab
Manuela Gomes <manudeveloper82@gmail.com>
2023-04-14 01:56:08 -0300

    tualização link

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -12,4 +12,5 @@ export class ContentComponent {
   cardDescription:string = "Os recursos de filmes e séries de TV mais recentes e populares estão todos no apk Youcine. Como plataforma de streaming de mídia, não há apenas um grande número de recursos de programas, mas também filmes e animações especiais para crianças. Ao mesmo tempo, nosso apk é adequado para celulares, TV e PC, permitindo que você assista aos mais recentes filmes e séries de TV em alta definição gratuitamente a qualquer hora e em qualquer lugar."
   cardLink:string = "https://www.youcine.net/"
 
+
 }
